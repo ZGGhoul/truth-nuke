69f3c1f93ae9dd79169d8dce5127cafb6af3a59e
ZGGhoul <kocziand@gmail.com>
2026-01-06 13:20:26 +0100

    hogolyo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { initializeApp } from "firebase/app";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      
+    <div className='app'>
+        <img src="https://www.rover.com/blog/wp-content/uploads/2019/04/cute-big-eyes-1024x682.jpg" alt="" />
     </div>
   )
 }
